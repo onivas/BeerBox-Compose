37c2cae61a798e685e8343606345b91aa575ed74
Ordine Savino <savino.ordine@pirelli.com>
2021-12-30 12:00:40 +0100

    detail view - quick UI

--- app/src/main/java/com/savinoordine/beerboxcompose/ui/detail/BeerDetailScreen.kt
+++ app/src/main/java/com/savinoordine/beerboxcompose/ui/detail/BeerDetailScreen.kt
@@ -1,15 +1,25 @@
 package com.savinoordine.beerboxcompose.ui.detail
 
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.requiredHeightIn
+import android.widget.Toast
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.livedata.observeAsState
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.RectangleShape
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
+import coil.compose.rememberImagePainter
 
 @Composable
 fun BeerDetailScreen(viewModel: BeerDetailViewModel = hiltViewModel()) {
@@ -23,9 +33,44 @@ fun BeerDetailScreen(viewModel: BeerDetailViewModel = hiltViewModel()) {
                     min = 250.dp, max = 500.dp
                 )
         ) {
-            Text(text = value.name)
+            Row(modifier = Modifier.fillMaxWidth()) {
+                Image(
+                    modifier = Modifier.height(150.dp),
+                    painter = rememberImagePainter(data = value.imageUrl),
+                    contentDescription = null
+                )
+                Column(modifier = Modifier.fillMaxWidth()) {
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = value.name,
+                        textAlign = TextAlign.Center
+                    )
+                    Text(
+                        modifier = Modifier.fillMaxWidth(),
+                        text = value.tagline,
+                        textAlign = TextAlign.Center,
+                        fontStyle = FontStyle.Italic,
+                        fontSize = 12.sp
+                    )
+                    Text(
+                        text = value.description,
+                        fontSize = 15.sp,
+                        color = Color.Blue,
+                        modifier = Modifier
+                            .padding(4.dp)
+                            .border(
+                                border = BorderStroke(width = 2.dp, color = Color.Blue),
+                                shape = RectangleShape
+                            )
+                            .padding(8.dp)
+                    )
+                }
+
+            }
         }
-    } ?: run {/* TODO */ }
+    } ?: run {
+        Toast.makeText(LocalContext.current, "Error", Toast.LENGTH_LONG).show()
+    }
 }
 
 
